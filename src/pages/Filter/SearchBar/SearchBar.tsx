6a00e22ac72e509f7fcf809c01361307464ed9ee
arapbaeva <aiymvector@gmail.com>
2023-03-14 19:51:32 +0600

    эфÐfix

--- src/pages/Filter/SearchBar/SearchBar.tsx
+++ src/pages/Filter/SearchBar/SearchBar.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 import './SearchBar.scss'
 import {AiOutlineSearch} from "react-icons/ai";
+import {useAppDispatch} from "../../../store/store";
+import {getPacks, setSearch} from "../../../store/packs-reducer";
+
+function useDebounce(value: string, delay: number = 800) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(
+        () => {
+            const handler = setTimeout(() => {
+                setDebouncedValue(value);
+            }, delay);
+
+            return () => {
+                clearTimeout(handler);
+            };
+        },
+        [value, delay]
+    );
+    return debouncedValue;
+}
+
 
 export const SearchBar = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const debouncedSearchTerm = useDebounce(searchTerm, 1500);
+    const dispatch = useAppDispatch();
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.currentTarget.value)
+    }
+
+    useEffect(
+        () => {
+            dispatch(setSearch(searchTerm))
+            dispatch(getPacks())
+        },
+        [debouncedSearchTerm]
+    );
+
     return (
         <div className={'search-container'}>
             <div className={'search-title b-title bt14 medium'}>Search</div>
             <div>
                 <input
+                    value={searchTerm}
+                    onChange={onChangeHandler}
                     type={'text'}
                     id={'search'}
                     placeholder={'Provide your text'}
